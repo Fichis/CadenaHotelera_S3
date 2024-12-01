ded63416fa244321bc4fa80fe232b24496ba9c97
Adolfo <adolfoinsti@gmail.com>
2024-12-01 10:58:52 +0100

    agregado output de la ip para facilidad

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,8 +13,8 @@ provider "aws" {
 }
 
 #? Creación de par de claves
-resource "aws_key_pair" "ubuntu-ssh" {
-  key_name   = "ubuntu-ssh"
+resource "aws_key_pair" "fedora-ssh" {
+  key_name   = "fedora-ssh"
   public_key = file("../my-key-pair.pub")
 
   tags = {
@@ -23,13 +23,13 @@ resource "aws_key_pair" "ubuntu-ssh" {
 }
 
 #? Creación de Instancia EC2
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "fedora" {
   ami               = "ami-0453ec754f44f9a4a"
   instance_type     = "t2.micro"
 
   user_data = file("./script.sh")
 
-  key_name = aws_key_pair.ubuntu-ssh.key_name
+  key_name = aws_key_pair.fedora-ssh.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.test.id
@@ -39,4 +39,9 @@ resource "aws_instance" "ubuntu" {
   tags = {
     Name = "Cadena_Hotelera"
   }
+}
+
+output "instance_public_ip" {
+  description = "IP pública de la instancia EC2"
+  value       = aws_instance.fedora.public_ip
 }

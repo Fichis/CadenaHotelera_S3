b11396cbb2caee7ff15d8b9598a8adf3360309ba
Adolfo <adolfoinsti@gmail.com>
2024-12-01 10:51:04 +0100

    arreglo de archivos

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-sudo apt update
-sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php -y
-sudo systemctl enable apache2
-sudo systemctl enable libapache2-mod-php
-sudo systemctl start apache2
-sudo systemctl start libapache2-mod-php
+#!bin/bash
+sudo dnf update -y
+sudo dnf install httpd php -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
